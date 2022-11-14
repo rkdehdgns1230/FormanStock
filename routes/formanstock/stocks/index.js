33d80bdea2802502e837786e7c918f1b5acd5dab
dongHun <actgong@gmail.com>
2022-11-14 16:11:23 +0900

    company data 추가 api 구현, 세부 주식 정보 백엔드 구현

--- routes/formanstock/stocks/index.js
+++ routes/formanstock/stocks/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const stockController = require('../../../controllers/stockController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/:stock_code', stockController.getSpecificStockInfo);
+router.post('/:stock_code/buy');
+router.post('/:stock_code/sell');
+// 임시로 company data 추가하는 api 추가
+router.get('/:stock_code/importCompanyInfo', stockController.loadCompanyData);
 
 module.exports = router;
