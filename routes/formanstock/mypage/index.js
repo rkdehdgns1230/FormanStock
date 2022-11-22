65bb195e19b9bd5a9efe89123602700b67c3a2e4
bnk159hair <bnk159hair@naver.com>
2022-11-23 00:58:44 +0900

    마이페이지 라우트 수정

--- routes/formanstock/mypage/index.js
+++ routes/formanstock/mypage/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const mypageControllers = require('../../../controllers/mypageControllers');
 const router = express.Router();
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('mypage/index', { title: 'Express' });
+// });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('mypage/index', { title: 'Express' });
-});
+
+
+router.get('/',mypageControllers.get_userINFO);
+
+// router.get('/companies',manage_controllers.getlist_company);
+// router.get('/users-suspension',manage_controllers.getlist_users_suspension);
+// router.get('/users',manage_controllers.getlist_users);
+// router.get('/comments',manage_controllers.getlist_comments);
+
+
+
 
 module.exports = router;
