d421ca35a1edc4457df92abb26b2966e50eeee09
dongHun <actgong@gmail.com>
2022-12-01 18:58:17 +0900

    auth middleware 등록

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ var router = express.Router();
 const formanstockRouter = require('./formanstock');
 const loginRouter = require('./login-page');
 const signUpRouter = require('./signup-page');
-
+const {auth} = require('../controllers/middleware/auth');
 // 각 router를 url path에 맞게 미들웨어로 등록
 // 각각의 미들웨어는 폴더를 통해 계층적으로 관리한다.
-router.use('/formanstock', formanstockRouter);
+// 임시로 /formanstock route에 auth 등록
+router.use('/formanstock', auth, formanstockRouter);
 router.use('/login-page', loginRouter);
 router.use('/signup-page',signUpRouter);
 
