586133ff66901aca0c3998300469f8d3eef23c7e
gunu <gunu9441@gmail.com>
2022-12-04 18:55:26 +0900

    [Upload]Upload sources

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -2,6 +2,16 @@ var boardModel = require('../models/boardModel')
 var express = require('express')
 
 exports.getPage=(req, res, next) => {
+    console.log(req.params.stock_code)
     console.log('게시글 페이지');
-    res.render('board/index',{title: '게시판'});
+    let loginSuccess = !(req.token === undefined);
+    let loginString= loginSuccess ? "success" : "fail"; 
+
+    boardModel.getPosts(req.params.stock_code, (rows)=>{
+        console.log("rows:" + JSON.stringify(rows));
+        res.render('board',{title:'게시물', rows:rows, userInfo: {
+            login: loginString,
+            info: loginSuccess ? req.row : 'empty'
+        }});
+    });
 };
