690784e2edaca9b28d50e31dfa5b4ddb5380fda1
bnk159hair <bnk159hair@naver.com>
2022-12-04 15:39:33 +0900

    mypage 인덱스

--- routes/formanstock/mypage/index.js
+++ routes/formanstock/mypage/index.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 
 
-router.get('/', middleware.auth, mypageControllers.get_userINFO);
+router.get('/:user_id', middleware.auth, mypageControllers.get_userINFO);
 router.post('/change_pw/:user_id', middleware.auth, mypageControllers.ChangePW);
 
 // router.get('/companies',manage_controllers.getlist_company);
