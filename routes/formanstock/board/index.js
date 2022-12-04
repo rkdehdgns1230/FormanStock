2425b590c0cfd1b5dc1326557d5a78ecdd81e523
HiYoung <71634377+bnk159hair@users.noreply.github.com>
2022-12-04 20:54:41 +0900

    게시판 기능

--- routes/formanstock/board/index.js
+++ routes/formanstock/board/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+var board_controllers = require('../../../controllers/boardController');
 /* GET home page. */
 
+
+
 router.get('/:stock_code/posts', ); // 종목토론 게시판 조회
-router.get('/:stock_code/write-form', ); // 게시글 작성
+router.get('/:stock_code/write-form', board_controllers.read_board); // 게시글 작성
+
 router.get('/:stock_code/posts/:post_no', ); // 게시글 조회
 
 
-router.post('/:stock_code/write-form', ); // 작성글 제출
+router.post('/:stock_code/write-form', board_controllers.write_board); // 작성글 제출
 router.post('/:stock_code/posts/:post_no', ); // 게시글 수정
 
 module.exports = router;
